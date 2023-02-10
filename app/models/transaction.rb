b33dc9ea54bd34a0054e9b7d8108e1a81c88d0f4
Oleh Sobchuk <oleh.sobchuk@seekingalpha.com>
2023-02-10 17:20:18 +0200

    Add metaprogramming for defining methods

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Transaction < ApplicationRecord
+  TYPES = %i[authorize charge refund reversal].freeze
+
   enum status: {
     pending: :pending,
     approved: :approved,
@@ -15,6 +17,12 @@ class Transaction < ApplicationRecord
   validates :amount, numericality: { greater_than: 0 }
 
   class << self
+    TYPES.each do |type| # just example for metaprogramming
+      define_method "type_#{type}" do
+        where(type: "#{name}s::#{type.to_s.capitalize}")
+      end
+    end
+
     def total_amount
       sum(:amount)
     end
